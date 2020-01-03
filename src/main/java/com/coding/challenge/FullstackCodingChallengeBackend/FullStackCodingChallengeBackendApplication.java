60980eea85186364003cd2ee7deb9003aeb0ca00
Otman Craay <otmancr0@gmail.com>
2020-01-03 10:48:34 +0100

    first commit

---
+++ src/main/java/com/coding/challenge/FullstackCodingChallengeBackend/FullStackCodingChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.FullstackCodingChallengeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackCodingChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackCodingChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/challenge/FullstackCodingChallengeBackend/FullStackCodingChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.challenge.FullstackCodingChallengeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCodingChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
