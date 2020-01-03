48a4d955c3239157cca03665013d068776dc4732
Otman Craay <otmancr0@gmail.com>
2020-01-03 11:13:09 +0100

    Creating Repositories for User and Role

---
+++ src/main/java/com/coding/challenge/FullstackCodingChallengeBackend/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.coding.challenge.FullstackCodingChallengeBackend.repository;
+
+import com.coding.challenge.FullstackCodingChallengeBackend.model.Role;
+import com.coding.challenge.FullstackCodingChallengeBackend.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
---
+++ src/main/java/com/coding/challenge/FullstackCodingChallengeBackend/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.coding.challenge.FullstackCodingChallengeBackend.repository;
+
+import com.coding.challenge.FullstackCodingChallengeBackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Boolean existsByEmail(String email);
+}
