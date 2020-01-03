98107fd50f85a578b394efcca1ff4eb145770619
Otman Craay <otmancr0@gmail.com>
2020-01-03 11:40:00 +0100

    Configure Spring Security along with JWT authentication

--- src/main/java/com/coding/challenge/FullstackCodingChallengeBackend/model/User.java
+++ src/main/java/com/coding/challenge/FullstackCodingChallengeBackend/model/User.java
@@ -19,6 +19,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
     @NaturalId
     @NotBlank
     @Size(max = 40)
@@ -50,6 +54,14 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
